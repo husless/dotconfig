63a3bd9f70c0b4a0bf54c7b644d0e63466cb7e10
Nolan <2921692+husless@users.noreply.github.com>
2023-04-04 23:02:40 +0800

    customize

---
+++ lua/plugins/colorscheme.lua
@@ -0,0 +1,8 @@
+return {
+  {
+    "folke/tokyonight.nvim",
+    opts = {
+      style = "night", -- The theme comes in three styles, `storm`, `moon`, a darker variant `night` and `day`
+    },
+  },
+}
