47bb8356aa20e9243e3652f0c23ef76a03c75829
Nolan <2921692+husless@users.noreply.github.com>
2024-05-05 21:41:02 +0800

    colortheme transparent

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -3,11 +3,6 @@ return {
     "folke/tokyonight.nvim",
     opts = {
       style = "night", -- The theme comes in three styles, `storm`, `moon`, a darker variant `night` and `day`
-      transparent = true,
-      styles = {
-        sidebars = "transparent",
-        floats = "transparent",
-      },
     },
   },
 }
