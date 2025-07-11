5d2f922199ad2073d735cb7cc6334ed0e8e9b16a
Nolan <2921692+husless@users.noreply.github.com>
2025-07-11 20:22:42 +0800

    render markdown

--- lua/plugins/ui.lua
+++ lua/plugins/ui.lua
@@ -60,6 +60,18 @@ return {
     end,
   },
   {
+    "MeanderingProgrammer/render-markdown.nvim",
+    event = "VeryLazy",
+    -- dependencies = { "nvim-treesitter/nvim-treesitter", "echasnovski/mini.nvim" }, -- if you use the mini.nvim suite
+    dependencies = { "nvim-treesitter/nvim-treesitter", "echasnovski/mini.icons" }, -- if you use standalone mini plugins
+    -- dependencies = { 'nvim-treesitter/nvim-treesitter', 'nvim-tree/nvim-web-devicons' }, -- if you prefer nvim-web-devicons
+    ---@module 'render-markdown'
+    ---@type render.md.UserConfig
+    opts = {
+      completions = { blink = { enabled = true }, lsp = { enabled = true } },
+    },
+  },
+  {
     "folke/snacks.nvim",
     opts = {
       dashboard = {
