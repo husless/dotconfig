2a9c4027d033ea294f612537a45bdcae5ad9cc6c
Nolan <2921692+husless@users.noreply.github.com>
2025-07-12 23:13:27 +0800

    lazily load markdown render

--- lua/plugins/cmp.lua
+++ lua/plugins/cmp.lua
@@ -10,7 +10,7 @@ return {
       -- 'super-tab' for mappings similar to vscode (tab to accept, arrow keys to navigate)
       -- 'enter' for mappings similar to 'super-tab' but with 'enter' to accept
       -- See the full "keymap" documentation for information on defining your own keymap.
-      keymap = { preset = "super-tab" },
+      keymap = { preset = "enter" },
     },
   },
 }
